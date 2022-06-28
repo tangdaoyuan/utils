ed14f397892d017cb6643183b0c8fa67c78ffc86
唐道源 <tangdaoyuan@megvii.com>
2022-06-28 11:10:40 +0800

    feat(collection): add recursive find & sort

---
+++ src/collection/index.ts
@@ -0,0 +1,69 @@
+import { cloneDeep } from '@/lang'
+
+/**
+ *
+ * @param items 用于遍历的数组
+ * @param key 用于排序的关键字
+ * @param childKey 子节点关键字
+ * @returns
+ */
+export function recursiveSort<T>(
+  items: T[],
+  key: keyof T,
+  childKey: keyof T,
+): T[] {
+  const _items = cloneDeep(items)
+
+  function _recursive(children: T[]) {
+    if (!children.length)
+      return
+
+    children.forEach((item) => {
+      const itemChildren = (item?.[childKey] as unknown) as T[]
+      if (Array.isArray(itemChildren) && itemChildren.length)
+        _recursive(itemChildren)
+    })
+    children.sort(
+      (a, b) =>
+        ((a[key] as unknown) as number) - ((b[key] as unknown) as number),
+    )
+  }
+  try {
+    _recursive(_items)
+  }
+  catch (error) {
+    console.error(error)
+    return items
+  }
+  return _items
+}
+
+/**
+ *
+ * @param items 用于遍历的数组
+ * @param search 查询内容
+ * @param key 查询字段
+ * @param childKey 子节点关键字
+ * @returns
+ */
+export function recursiveFind<T>(
+  items: T[],
+  search: string | number,
+  key: keyof T,
+  childKey: keyof T,
+): T | null {
+  function _recursive(children: T[]): T | null {
+    for (const item of children) {
+      if (((item[key] as unknown) as string | number) === search)
+        return item
+
+      if (Array.isArray(item?.[childKey])) {
+        const ret = _recursive((item[childKey] as unknown) as T[])
+        if (ret)
+          return ret
+      }
+    }
+    return null
+  }
+  return _recursive(items)
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,69 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { recursiveFind, recursiveSort } from '@/collection'
 
-it('runs', () => {
-  expect(true).toBe(true)
+describe('test', () => {
+  it('sort collections recursively by KEY', () => {
+    const collections = [
+      { id: 5, children: [{ id: 2 }, { id: 1 }] },
+      { id: 1, children: [{ id: 2 }] },
+      { id: 3, children: [{ id: 4 }] },
+    ]
+
+    const ans = recursiveSort(collections, 'id', 'children')
+    expect(ans).not.toBeNull()
+    expect(ans).toMatchInlineSnapshot(`
+      [
+        {
+          "children": [
+            {
+              "id": 2,
+            },
+          ],
+          "id": 1,
+        },
+        {
+          "children": [
+            {
+              "id": 4,
+            },
+          ],
+          "id": 3,
+        },
+        {
+          "children": [
+            {
+              "id": 1,
+            },
+            {
+              "id": 2,
+            },
+          ],
+          "id": 5,
+        },
+      ]
+    `)
+  })
+  it('find target recursively from collections', () => {
+    const collections = [
+      { id: 5, children: [{ id: 2 }, { id: 1 }] },
+      { id: 1, children: [{ id: 2 }] },
+      { id: 3, children: [{ id: 4 }] },
+    ]
+
+    const ans = recursiveFind(collections, 5, 'id', 'children')
+    expect(ans).not.toBeNull()
+    expect(ans).toMatchInlineSnapshot(`
+      {
+        "children": [
+          {
+            "id": 2,
+          },
+          {
+            "id": 1,
+          },
+        ],
+        "id": 5,
+      }
+    `)
+  })
 })
